10b8e77851730384f4d4cd244a83a103aa4fddc3
Victor Hermes <me.victorhermes@gmail.com>
2019-03-07 20:51:56 -0300

    Hook

--- app/Models/Task.js
+++ app/Models/Task.js
@@ -1,9 +1,15 @@
 'use strict'
 
-/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
 class Task extends Model {
+  static boot () {
+    super.boot()
+
+    this.addHook('afterCreate', 'TaskHook.sendNewTaskMail')
+    this.addHook('beforeUpdate', 'TaskHook.sendNewTaskMail')
+  }
+
   project () {
     return this.belongsTo('App/Models/Project')
   }
